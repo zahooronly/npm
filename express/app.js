b9cd6bef65f63c581f093d5d4d4454c8e5458cb4
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-06-27 10:26:28 +0500

    http get request

--- express/app.js
+++ express/app.js
@@ -1,5 +1,28 @@
 const express = require("express");
 const app = express();
+// app.use(express.json);
+const courses = [
+  {
+    id: 1,
+    name: "course 1",
+  },
+  {
+    id: 2,
+    name: "course 2",
+  },
+  {
+    id: 3,
+    name: "course 3",
+  },
+  {
+    id: 4,
+    name: "course 4",
+  },
+  {
+    id: 5,
+    name: "course 5",
+  },
+];
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
@@ -9,7 +32,10 @@ app.get("/api", (req, res) => {
 });
 
 app.get("/api/courses/:id", (req, res) => {
-  res.send(req.params.id);
+  // res.send(req.params.id);
+  const course = courses.find((a) => a.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("Course doesn't exist");
+  res.send(course);
 });
 // app.get("/api/articles/:year/:month/:day", (req, res) => {
 //   res.send(req.params);
@@ -17,5 +43,15 @@ app.get("/api/courses/:id", (req, res) => {
 app.get("/api/articles/:year/:month/:day", (req, res) => {
   res.send(req.query);
 });
-const port = process.env.PORT || 3000;
+
+// app.post("api/courses", (req, res) => {
+//   const course = {
+//     id: courses.length + 1,
+//     name: req.body.name,
+//   };
+//   courses.push(course);
+//   res.send(course);
+// });
+
+const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`listening on port ${port}...`));

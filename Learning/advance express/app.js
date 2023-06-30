87678d2704c2a0f4b19ff94a3c030704c2c68359
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-06-30 11:42:32 +0500

    debug config helmet morgan middlewares

--- Learning/advance express/app.js
+++ Learning/advance express/app.js
@@ -1,10 +1,36 @@
+const appDebugger = require("debug")("app:startup");
+const dbDebugger = require("debug")("app:db");
+const config = require("config");
 const Logger = require("./logger");
+const helmet = require("helmet");
+const morgan = require("morgan");
 const Joi = require("joi");
+// throw new Error('Configuration property "' + property + '" is not defined');
 const express = require("express");
 const app = express();
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+// Configuration
+console.log(`Name: ${config.get("name")}`);
+console.log(`Host: ${config.get("mail.host")}`);
+console.log(`Auth: ${config.get("mail.auth.user")}`);
+// console.log(`username: ${config.get("auth.user")}`);
+// console.log(`password: ${config.get("mail.auth.password")}`);
+dbDebugger("Connected to database...");
+app.use(helmet());
 app.use(express.static("public"));
 app.use(Logger);
 app.use(express.json());
+
+// console.log(`ENV is: ${app.get("env")}`);
+// console.log(`ENV is: ${process.env.NODE_ENV}`);
+
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+  appDebugger("Morgan enabled...");
+}
+
 const courses = [
   {
     id: 1,
@@ -28,7 +54,8 @@ const courses = [
   },
 ];
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  // res.send("Hello World");
+  res.render("app", { title: "My App", message: "Hello", message2: "World" });
 });
 
 app.get("/api", (req, res) => {

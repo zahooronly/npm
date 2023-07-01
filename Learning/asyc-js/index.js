f78b48f8b8f4825b02dc441db64bb0ea763c3f8a
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-07-01 11:31:03 +0500

    callback hell

--- Learning/asyc-js/index.js
+++ Learning/asyc-js/index.js
@@ -4,8 +4,15 @@ getUser(1, (user) => {
   console.log(user);
   getRepos(user.gitUsername, (repos) => {
     console.log("Repos: ", repos);
+    getSuperUsedRepo(repos.MostUsedRepo, (used) => {
+      console.log(used);
+      getCommits(used.commits, (commits) => {
+        console.log("Commits: ", commits);
+      });
+    });
   });
 });
+// So this is a callback hell: We've to resolve it
 // getRepos("ZahoorOnly", (repos) => {
 //   console.log("Repos: ", repos);
 // });

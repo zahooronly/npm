82c7c2ce7d3eb4ec6198f9ad354cf41ecf2650ba
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-07-01 11:25:43 +0500

    callback functions

--- Learning/asyc-js/index.js
+++ Learning/asyc-js/index.js
@@ -1,5 +1,34 @@
 console.log("one");
-setTimeout(() => {
-  console.log("setTimeout");
-}, 2000);
+// const user = getUser(1);
+getUser(1, (user) => {
+  console.log(user);
+  getRepos(user.gitUsername, (repos) => {
+    console.log("Repos: ", repos);
+  });
+});
+// getRepos("ZahoorOnly", (repos) => {
+//   console.log("Repos: ", repos);
+// });
+// console.log(user);
 console.log("two");
+
+function getUser(id, callback) {
+  console.log("Geting User...");
+  setTimeout(() => {
+    callback({
+      id: id,
+      gitUsername: "ZahoorOnly",
+    });
+    // return;
+  }, 2000);
+}
+function getRepos(username, callback) {
+  setTimeout(() => {
+    console.log("Calling GitHub API...");
+    callback({
+      username: username,
+      Repos: ["repo1", "repo2", "repo3"],
+    });
+    // return;
+  }, 2000);
+}
---
+++ Learning/asyc-js/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+Only", (repos) => {
+//   console.log("Repos: ", repos);
+// });

845993be58ccd2c019293b65ff571ae2443114b8
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-07-02 13:37:56 +0500

    connected to database

--- Learning/mongodb/index.js
+++ Learning/mongodb/index.js
@@ -3,3 +3,28 @@ mongoose
   .connect("mongodb://localhost/Learning")
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Couldn't connected to MongoDB"));
+
+// Schemas
+
+const courseShema = new mongoose.Schema({
+  name: String,
+  auther: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
+// Models
+const Course = mongoose.model("Course", courseShema);
+// course Object of Course
+const createCourse = async () => {
+  const course = new Course({
+    name: "Node.JS",
+    auther: "ZahoorOnly",
+    tags: ["node", "ZahoorOnly", "backend", "mongodb", "mongoose"],
+    isPublished: true,
+  });
+
+  const result = await course.save();
+  console.log(result);
+};
+createCourse();

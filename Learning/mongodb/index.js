7e6a80c51fb49e6feb69232152b06abb7e2f2676
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-07-02 15:44:16 +0500

    CR=Create Read/Retrieve

--- Learning/mongodb/index.js
+++ Learning/mongodb/index.js
@@ -16,15 +16,27 @@ const courseShema = new mongoose.Schema({
 // Models
 const Course = mongoose.model("Course", courseShema);
 // course Object of Course
+// CRUD Operations
+// C=> Create
 const createCourse = async () => {
   const course = new Course({
-    name: "Node.JS",
+    name: "Backend",
     auther: "ZahoorOnly",
-    tags: ["node", "ZahoorOnly", "backend", "mongodb", "mongoose"],
+    tags: ["backend", "ZahoorOnly", "Prizma", "backend", "mongodb", "mongoose"],
     isPublished: true,
   });
 
   const result = await course.save();
   console.log(result);
 };
-createCourse();
+// createCourse();
+// R=> Read/Retrieve
+
+async function getCourses() {
+  const courses = await Course.find({ auther: "ZahoorOnly", isPublished: true })
+    .limit(10)
+    .select({ name: 1, tags: 1, auther: 1 })
+    .sort({ name: 1 });
+  console.log("All Courses:\n", courses);
+}
+getCourses();

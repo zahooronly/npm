18c80818c429b4b5fb3b39ca5d12508ad9126916
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-07-03 10:50:17 +0500

    validation

--- Learning/mongodb/tempCodeRunnerFile.js
+++ Learning/mongodb/tempCodeRunnerFile.js
@@ -1,2 +1,2 @@
-result = await course.save();
-    // consol
+await course.save();
+    console.log(res
--- Learning/mongodb/validationIndex.js
+++ Learning/mongodb/validationIndex.js
@@ -7,12 +7,32 @@ mongoose
 // Schemas
 
 const courseSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, minlength: 5, maxlength: 50 },
   auther: String,
-  tags: [String],
+  category: {
+    type: String,
+    required: true,
+    enum: ["web", "mobile", "desktop"],
+  },
+  tags: {
+    type: Array,
+    validate: {
+      validator: function (value) {
+        return value && value.length > 0;
+      },
+      message: "A course should have at least one tag",
+    },
+  },
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
-  price: Number,
+  price: {
+    type: Number,
+    required: {
+      function() {
+        return this.isPublished;
+      },
+    },
+  },
 });
 // Models
 const Course = mongoose.model("Course", courseSchema);
@@ -21,58 +41,35 @@ const Course = mongoose.model("Course", courseSchema);
 // C=> Create
 const createCourse = async () => {
   const course = new Course({
-    name: "Backend",
+    name: "Prisma",
     auther: "ZahoorOnly",
-    tags: ["backend", "ZahoorOnly", "Prisma", "backend", "mongodb", "mongoose"],
+    tags: ["Zahoor Ahmad", "Prisma", "backend", "mongodb", "mongoose"],
     isPublished: true,
+    price: 23,
+    category: "wEb".toLowerCase(),
   });
   try {
-    await course.validate();
-    // const result = await course.save();
-    // console.log(result);
+    async function Validation() {
+      // await course.validate();
+      const result = await course.save();
+      console.log(result);
+    }
+    Validation();
   } catch (ex) {
     console.log(ex.message);
   }
 };
-// createCourse();
+createCourse();
 
 // R=> Read/Retrieve
 
-// async function getCourses() {
-//   const courses = await Course.find({ auther: "ZahoorOnly", isPublished: true })
-//     .limit(10)
-//     .select({ name: 1, tags: 1, auther: 1 })
-//     .sort({ name: 1 });
-//   console.log("All Courses:\n", courses);
-// }
 async function getCourses() {
-  const courses = await Course
-
-    // simple find function in Mongoose
-    // .find({ auther: "ZahoorOnly", isPublished: true })
-
-    // Comparison Operators {=,<,>,<>,!=,=<,=>}
-    // .find({price:{$in:[10,15,20]}})
-
-    // Logical Operators {and,or}
-    .find()
-    // .or({ auther: "ZahoorOnly" }, { isPublished: true })
-
-    // Regular Expressions
-    // Starts with
-    // .find({ auther: /^Zahoor/i })
-    // Ends with
-    // .find({ auther: /Only$/i })
-    // Contains
-    // .find({ auther: /.*oor.*/i })
-    // after slash / character "i" indicates case insensitivity
-
-    .limit(10)
+  const courses = await Course.find()
     .select({ name: 1, tags: 1, auther: 1 })
     .sort({ name: 1 });
   console.log("All Courses:\n", courses);
 }
-getCourses();
+// getCourses();
 
 // U=> Update
 // Query First Approach
@@ -108,9 +105,6 @@ async function directUpdateCourse() {
 // D=> Delete {CRUD completed}
 async function deleteCourse(ID) {
   const result = await Course.deleteOne({ _id: ID });
-  // if want to get the deleted course, then we can use
-  //  findByIdAndRemove
-  // Course.findByIdAndRemove
   console.log("Deleted Course:\n", result);
 }
 // deleteCourse("64a152a1cb6a55e187aa7ddd");
